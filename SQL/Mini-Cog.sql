fec5472d9dc19030accc54175ac038e3426c0b92
Eli Gubbins <112986003+EliGubbins@users.noreply.github.com>
2026-02-23 20:36:38 -0600

    Add SQL query to return Mini-Cog results

---
+++ SQL/Mini-Cog.sql
@@ -0,0 +1,9 @@
+--return minicog results
+SELECT m.value_as_number,
+ml.concept_name,
+m.measurement_datetime,
+m.patient_id
+FROM measurement m
+JOIN measurement_lookup ml
+ON m.measurement_concept_id = ml.measurement_concept_id
+WHERE m.measurement_concept_id = 37017073
