65c69c024030f6e36534daaa4f053a1b0e8a6f4b
EliGubbins <eligubbins@gmail.com>
2026-03-06 21:17:37 -0600

    Update Outcome Queries to be percentage based

--- SQL/Mini-Cog.sql
+++ SQL/Mini-Cog.sql
@@ -1,9 +1,9 @@
---return minicog results
-SELECT m.value_as_number,
+--return minicog results as a percentage
+SELECT m.value_as_number / 5 * 100 as PercentScore,
 ml.concept_name,
 m.measurement_datetime,
 m.patient_id
 FROM measurement m
 JOIN measurement_lookup ml
 ON m.measurement_concept_id = ml.measurement_concept_id
-WHERE m.measurement_concept_id = 37017073
+WHERE m.measurement_concept_id = 37017073 and m.value_as_number is not null
